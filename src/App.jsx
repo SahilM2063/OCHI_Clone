05b287ecb116ad0c0f60e10223f814f59665a36e
SAHIL MAKADIA <makadiasahil9846@gmail.com>
2024-02-12 22:06:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-[#F1F1F1]">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
